ae15a77f30627527c268d1ffcb817edd0f9111fa
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-16 10:10:26 +0300

    Update itc_same_parts_list.cpp

--- itc_same_parts_list.cpp
+++ itc_same_parts_list.cpp
@@ -1,8 +1,8 @@
 #include "easy_list.h"
 
-bool itc_same_parts_list(const vector <int> &mass){
-    for (int i = 1; i < mass.size(); i ++){
-        if ((mass[i] < 0 && mass[i - 1] < 0) || (mass[i] >= 0 && mass[i - 1] >= 0))
+bool itc_same_parts_list(const vector <int> &myvector){
+    for (int i = 1; i < myvector.size(); i = i + 1){
+        if (((myvector[i] < 0) && (myvector[i - 1] < 0)) || ((myvector[i] >= 0) && (myvector[i - 1] >= 0)))
              return true;
     }
     return false;
