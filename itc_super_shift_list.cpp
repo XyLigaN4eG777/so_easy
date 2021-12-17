501d5f6d8994894f105e09b3cd0fd91126648912
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-17 10:27:33 +0300

    Update itc_super_shift_list.cpp

--- itc_super_shift_list.cpp
+++ itc_super_shift_list.cpp
@@ -1,15 +1,16 @@
 #include "easy_list.h"
 
 void itc_super_shift_list(vector <int> &myvector, int a){
+    if (myvector.size() > 0){
     if (a < 0){
-        a = -1 * a;
-        for (int i = 0; i < a % myvector.size(); i = i + 1){
+        a = - 1 * a;
+        for (int i = 0; i < (a % myvector.size()); i = i + 1)
             itc_lshift_list(myvector);
-        }
     }
     else{
-        for (int i = 0; i < a % myvector.size(); i = i + 1){
+        for (int i = 0; i < (a % myvector.size()); i = i + 1){
             itc_rshift_list(myvector);
         }
+    }
     }
 }
