3a53893cc9dec0a16a6fd116bb98c863132d54ad
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-17 10:28:59 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -3,7 +3,7 @@
 void itc_rev_par_list(vector <int> &myvector){
     int a;
     a = 0;
-    for (int i = 1; i < mass.size(); i = i + 2){
+    for (int i = 1; i < myvector.size(); i = i + 2){
         a = myvector[i];
         myvector[i] = myvector[i - 1];
         myvector[i - 1] = a;
