2ff861aeff52fe3f053f629dd79f6017922c089a
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-20 11:31:57 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -1,11 +1,15 @@
 #include "easy_list.h"
 
-void itc_rev_par_list(vector <int> &myvector){
-    int a;
-    a = 0;
-    for (int i = 1; i < myvector.size(); i = i + 2){
-        a = myvector[i];
-        myvector[i] = myvector[i - 1];
-        myvector[i - 1] = a;
+void itc_rev_par_list(vector <int> &mass){
+    int c = mass.size();
+    int b;
+    if(c != 0){
+        for(int i = 0; i < c; i = i + 2){
+            if(i < count - 1){
+                b = mass[i];
+                mass[i] = mass[i + 1];
+                mass[i + 1] = b;
+            }
+        }
     }
 }
