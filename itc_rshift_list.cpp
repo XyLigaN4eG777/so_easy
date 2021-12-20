3ca5a5de6f5456383a5714c4e711c55e1b6b9249
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-20 11:24:26 +0300

    Update itc_rshift_list.cpp

--- itc_rshift_list.cpp
+++ itc_rshift_list.cpp
@@ -1,11 +1,13 @@
 #include "easy_list.h"
 
-
-void itc_rshift_list(vector <int> &myvector){
-    int g = myvector.size() - 1;
-    int a = myvector[g];
-    for (int i = myvector.size() - 2; i >= 0; i--){
-        myvector[i + 1] = myvector[i];
+void itc_rshift_list(vector <int> &mass){
+    int c = mass.size();
+    vector<int> mass2 = mass;
+    if(c != 0){
+        for(int i = 1; i < c; i = i + 2){
+            mass[i] = mass2[i - 1];
+            mass[i + 1] = mass2[i];
+        }
+        mass[0] = mass2[count - 1];
     }
-    myvector[0] = a;
 }
