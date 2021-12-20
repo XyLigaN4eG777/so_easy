de23df22dbc6d486142dfc2ba404f6e523e47c5c
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-20 11:18:22 +0300

    Update itc_positive_list.cpp

--- itc_positive_list.cpp
+++ itc_positive_list.cpp
@@ -1,11 +1,15 @@
 #include "easy_list.h"
 
-int itc_positive_list(const vector <int> &myvector){
+int itc_positive_list(const vector <int> &mass){
     int a;
-    a = 0;
-    for (int i = 0; i < myvector.size(); i = i + 1){
-        if (myvector[i] > 0)
-             a = a + 1;
+    a = mass.size();
+    int b = 0;
+    if(a != 0){
+        for(int i = 0; i < a; i++){
+            if(mass[i] >= 0){
+                b = b + 1;
+            }
+        }
     }
-    return a;
+    return b;
 }
