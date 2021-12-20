42aed07d05b952b47907ef760a58d07f479afa9a
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-20 11:41:51 +0300

    Update itc_rev_par_list.cpp

--- itc_rev_par_list.cpp
+++ itc_rev_par_list.cpp
@@ -5,7 +5,7 @@ void itc_rev_par_list(vector <int> &mass){
     int b;
     if(c != 0){
         for(int i = 0; i < c; i = i + 2){
-            if(i < count - 1){
+            if(i < c - 1){
                 b = mass[i];
                 mass[i] = mass[i + 1];
                 mass[i + 1] = b;
