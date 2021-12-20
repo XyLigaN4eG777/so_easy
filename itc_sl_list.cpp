5e0c390c1a2ce4c925ce64816c22a72cb9ba2d2a
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-20 11:33:35 +0300

    Update itc_sl_list.cpp

--- itc_sl_list.cpp
+++ itc_sl_list.cpp
@@ -1,11 +1,14 @@
 #include "easy_list.h"
 
-int itc_sl_list(const vector <int> &myvector){
+int itc_sl_list(const vector <int> &mass){
+    int c = mass.size();
     int a = 0;
-    for (int i = 1; i < myvector.size(); i = i + 1){
-        if (myvector[i] > myvector[i - 1])
-             a ++;
+    if(c != 0){
+        for(int i = 0; i < c - 1; i = i + 1){
+            if(mass[i + 1] > mass[i]){
+                a = a + 1;
+            }
+        }
     }
     return a;
 }
-
