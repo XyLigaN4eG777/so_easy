c67fc898332d38a140271a698bdbfc580eb5185e
XyLigaN4eG777 <92299169+XyLigaN4eG777@users.noreply.github.com>
2021-12-20 11:10:08 +0300

    Update itc_even_index_list.cpp

--- itc_even_index_list.cpp
+++ itc_even_index_list.cpp
@@ -1,9 +1,9 @@
 #include "easy_list.h"
 
 void itc_even_index_list(const vector <int> &mass, vector <int> &mass_2){
-    int count = mass.size();
-    if(count != 0){
-        for(int i = 0; i < count; i = i + 2){
+    int a = mass.size();
+    if(a != 0){
+        for(int i = 0; i < a; i = i + 2){
             mass_2.push_back(mass[i]);
         }
     }
